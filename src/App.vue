58975ebdc4352ad95f3bac8cee62601ff363e65f
Evgeny <gena@regimov.net>
2023-11-21 20:15:35 +0100

    new features

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/Main.vue'
 <!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
 
     <div class="wrapper">
-      <HelloWorld msg="FBminiBT Configurator" />
+      <HelloWorld msg="FlyBeeper mini BT Configurator" />
 
       <nav>
         <RouterLink to="/">Settings</RouterLink>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>FBminiBT - variometer with Bluetooth. Audible indication of climb rate.</p>
+    <p>FlyBeeper mini BT - variometer with Bluetooth. Audible indication of climb rate.</p>
     <p>This resource allows you to connect to the device over the air using web bluetooth technology and change its settings.</p>
     <p>Please, visit <a href="https://market.flybeeper.com/device/mini-bt">market</a> to read more.</p>
   </div>
